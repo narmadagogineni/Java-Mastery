5942c56f111ab81a42458c16ffb2c498ef05da8e
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-10-28 19:17:36 +0530

    Create javafiles.java

---
+++ javafiles.java
@@ -0,0 +1,17 @@
+public class javabasics {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Narmada Gogineni";
+        System.out.println(name);
+
+        a = 50;
+        System.out.println(a);
+        a = b;
+        System.out.println(a);
+    };
+};
