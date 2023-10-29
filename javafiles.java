a5dbc7a127346b7aa4f469f9ee8d412a3e52c686
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-10-29 14:51:12 +0530

    Update javafiles.java

--- javafiles.java
+++ javafiles.java
@@ -343,9 +343,22 @@ Source Code.java -> Compiler -> Byte Code.class -> Java Virtual Machine -> Nativ
            Compilation                                      Execution
         
 =========================================================================================================
-    
+ //OPERATORS   
 
+import java.util.*;
 
+public class javabasics {
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Add=" + (a+b));
+        System.out.println("Sub=" + (a-b));
+        System.out.println("Mul=" + (a*b));
+        System.out.println("Div=" + (a/b));
+        System.out.println("Mod=" + (a%b));
+    }
+}
 
 
 
