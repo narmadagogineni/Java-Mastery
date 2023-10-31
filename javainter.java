d7a75447d8776b7346d5f9dce43bc15bf96a6dfc
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-10-31 14:58:37 +0530

    Update javainter.java

--- javainter.java
+++ javainter.java
@@ -118,6 +118,44 @@ public class javabasics {
         System.out.println("Sum is : " + sum); 
     }
 }
+--------------------------------------------------------------
+    //reveser the given num in while loop
+    
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        int n = 9102001;
+
+        while(n > 0) {
+            int lastDig = n % 10;
+            System.out.print(lastDig);
+            n /= 10;
+        }
+        System.out.println();
+    }
+}
+
+-------------------------------------------------------
+// Method 2 : Reverse the num 
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        int num = 9102001;
+        int rev = 0;
+
+        while(num>0) {
+            int lastDigit = num%10;
+            rev = (rev*10) + lastDigit;
+            num /= 10;
+        }
+        System.out.println(rev);
+    }
+}
 ====================================================================================
     //FOR LOOP
 
@@ -165,9 +203,144 @@ public class javabasics {
         }
     }
 }
+-----------------------------------------------------------------------
+    //REVERSE THE GIVEN NUMBERS
+
+    //to get remainder num%10 - ex 24%10 = remainder 4
+    // ro remove last digit num/10 - ex 24/10 = 4 remainder and 2 is the remainding digit
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        int n = 16300;
+
+        for(;n>0; n/=10) {
+            int lastDig = n%10;
+            System.out.print(lastDig);
+        }
+        System.out.println();
+        }
+    }
+----------------------------------------------------------------------------------------------
+//REVERSE THE GIVEN NUMBER USING FOR LOOP
+    
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        int num = 9102001;
+        int rev = 0;
+
+        for(;num>0; num /= 10) {
+            int lastDigit = num%10;
+            rev = (rev*10) + lastDigit;
+        }
+        System.out.println(rev);
+    }
+}
+
+---------------------------------------------------------------------
+//DO-WHILE LOOP
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Hello world!");
+            i++;
+        } while (i<=5);
+    }
+}
+==============================================================================================
+    //BREAK STATEMENT
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++) {
+            if(i == 5) {
+               break;
+            }
+             System.out.println(i);
+        }
+        System.out.println("Im out of the loop!");
+    }  
+}
+--------------------------------------------------------
+    //Break the loop on entering a num divisble by 10
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("Enter a number n: ");
+            int n = sc.nextInt();
+            if(n % 10 == 0) {
+                break;
+            }
+            System.out.println(n);
+        } while(true);
+    }  
+}
+===========================================================================================
+//continue statement
+
+    //print 1 - 10 skip 5
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=1; i<=10; i++) {
+            if(i == 5) {
+                System.out.println("I have skipped 5");
+                continue;       
+            }
+            System.out.println(i);
+        }
+    }  
+}
+--------------------------------------------------------------
+// print all number n skip the num divisible by 10
+
+import java.util.*;
+
+public class javabasics {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("Enter a num: ");
+            int n = sc.nextInt();
+            if(n%10 == 0) {
+                System.out.println("Number is divisible by 10");
+                continue;
+            }
+            System.out.println("You entered: " +n);
+        } while(true);
+ 
+    }  
+}
 
 
 
 
 
 
+    
