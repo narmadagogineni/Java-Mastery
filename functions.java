dc39a3df8de460e25e65a5b2119b337ebe249b6a
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-01 22:25:17 +0530

    Update functions.java

--- functions.java
+++ functions.java
@@ -141,6 +141,28 @@ public class functions {
         System.out.println("b is " + b);
     }
 }
+--------------------------------------------------------------------------------------------
+//product of a and b
+    
+import java.util.*;
+
+public class functions {
+
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = multiply(a, b);
+        System.out.println("Product of a and b is " + result);
+    }
+
+}
+-----------------------------------------------------------------------------------------
 
 
 
