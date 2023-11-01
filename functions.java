545572bc2aec88482234932ba3e29131bedfc59b
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-01 21:58:30 +0530

    Create functions.java

---
+++ functions.java
@@ -0,0 +1,146 @@
+//METHODS OR FUNCTIONS
+
+// func to print hello world
+
+import java.util.*;
+
+public class functions {
+
+    public static void printHello() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String args[]) {
+        printHello();
+    }
+}
+
+-------------------------------------------------------------------------------------
+  //NOTE: by default functions shd return a value so we use return keyword or else it thorws an error
+  //but void func doesnt return anything so we can skip return 
+
+import java.util.*;
+
+public class functions {
+
+    public static void printHello() {
+        System.out.println("Hello world!");
+        return;
+    }
+
+    public static void main(String args[]) {
+        printHello();
+    }
+}
+-------------------------------------------------------------
+  // assume it as an int func then return value shd be given in int form
+  
+import java.util.*;
+
+public class functions {
+
+    public static void printHello() {
+        System.out.println("Hello world!");
+        return 3;
+    }
+
+    public static void main(String args[]) {
+        printHello();
+    }
+}
+--------------------------------------------------------------------------------
+//Sum of a and b with void type
+
+import java.util.*;
+
+public class functions {
+
+    public static void printSum() {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println("Sum of a and b is " + sum);
+        return ;
+    }
+
+    public static void main(String args[]) {
+        printSum();
+    }
+}
+---------------------------------------------------------------
+//sum of a and b with int type
+  
+import java.util.*;
+
+public class functions {
+
+    public static int printSum(int a, int b) {    //parameters = formal parameters
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = printSum(a, b);     //arguments = actual parameters
+        System.out.println("Sum of a and b is " + sum);
+    }
+}
+===========================================================================
+Note:
+  //call by value - copy of a original value of variable is used in java
+//call by reference - original value of variable is used in cpp
+==============================================================================
+//swap two numbers
+  
+import java.util.*;
+
+public class functions {
+
+    public static void main(String args[]) {
+        //swap - values
+        int a = 2;
+        int b = 3;
+
+        //swap
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+    }
+}
+---------------------------------------------------------------------------
+//call by value example
+  //swap 
+  
+import java.util.*;
+
+public class functions {
+
+    public static void swap(int a, int b) {
+        //swap
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a is " + a);         //copy
+        System.out.println("b is " + b);
+    }
+
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 3;
+        swap(a, b);
+        System.out.println("a is " + a);        //original values remains same
+        System.out.println("b is " + b);
+    }
+}
+
+
+
