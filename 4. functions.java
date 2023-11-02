dc754dda9ddd2df33b0ae02b8ff99fa6146871aa
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-02 13:57:04 +0530

    Update 4. functions.java

--- 4. functions.java
+++ 4. functions.java
@@ -163,6 +163,59 @@ public class functions {
 
 }
 -----------------------------------------------------------------------------------------
+//Factorial of a number
+    
+import java.util.*;
+
+public class functions {
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for(int i=1; i<=n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        int result = factorial(6);
+        System.out.println(result);
+    }
+}
+---------------------------------------------------------------------------------
+//Binomial coefficient
+
+import java.util.*;
+
+public class functions {
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for(int i=1; i<=n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static int binCoeff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n/ (fact_r * fact_nmr);
+        return binCoeff;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binCoeff(5, 2));
+    }
+}
+
+
+
+
+
+
 
 
 
