e63da68045d354c8741770637ac8b76e743d2a08
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-06 18:43:16 +0530

    Update 3. patterns.java

--- 3. patterns.java
+++ 3. patterns.java
@@ -83,32 +83,6 @@ public class patterns {
         
     }
 }
-================================================================================
-
-
-import java.util.*;
-
-public class functions {
-
-    public static void decToBin(int decNum) {
-        int myNum = decNum;
-        int power = 0;
-        int binNum = 0;
-
-        while (decNum > 0) {
-            int rem = decNum % 2;
-            binNum = binNum + (rem * (int) Math.pow(10, power));
-            
-            power++;
-            decNum = decNum / 2;
-        }
-        System.out.println("binary of "+ myNum + " is " + binNum);
-    }
-
-    public static void main(String args[]) {
-        decToBin(11);
-    }
-}
 
 ============================================================================================
 
@@ -138,8 +112,85 @@ public class functions {
         hallowRectangle(4, 5);
     }
 }
+--========================================================================================================
 
+//Right angle triangle
+    
+import java.util.*;
 
+public class functions {
+                //matrix application
+    public static void rightAngleTraingle(int n) {
+        //number of rows
+        for(int i=1; i<=n; i++) {
+            //to print spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            } 
+            //to print stars
+            for(int j=1; j<=i; j++) {
+                System.out.print ("* ");
+            }
+                //for nextline
+            System.out.println();
+        }  
+          
+    }
+
+    public static void main(String args[]) {
+      rightAngleTraingle(5);
+    }
+}
+
+==========================================================================
+
+    //inverted left angle traingle with repeatative numbers
+
+import java.util.*;
+
+public class functions {
+                //matrix application
+    public static void invertedLeftAngleNumberTraingle(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(j+" ");
+            } 
+
+            System.out.println();
+        }  
+          
+        }
+
+    public static void main(String args[]) {
+        invertedLeftAngleNumberTraingle(5);
+    }
+}
+===============================================================================================
+//Floyd's Triangle
+
+import java.util.*;
+
+public class functions {
+    //matrix application
+    public static void floydsTraingle(int n) {
+        int counter = 1;
+        //outer - rows
+        for(int i=1; i<=n; i++) {
+           for(int j=1; j<=i; j++){
+            System.out.print(counter+" ");
+            counter++;
+           }
+
+            System.out.println();
+        }  
+          
+        }
+
+    public static void main(String args[]) {
+        floydsTraingle(5);
+    }
+}
+============================================================================================
 
 
 
