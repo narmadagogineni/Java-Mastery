8e919700008e37368774421295c83e542b6f4204
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-06 12:48:39 +0530

    Update 5. binary and decimal.java

--- 5. binary and decimal.java
+++ 5. binary and decimal.java
@@ -25,3 +25,29 @@ public class functions {
 }
 
 ================================================================================================
+
+//DECIMAL TO BINARY
+
+import java.util.*;
+
+public class functions {
+
+    public static void decToBin(int decNum) {
+        int myNum = decNum;
+        int power = 0;
+        int binNum = 0;
+
+        while (decNum > 0) {
+            int rem = decNum % 2;
+            binNum = binNum + (rem * (int) Math.pow(10, power));
+            
+            power++;
+            decNum = decNum / 2;
+        }
+        System.out.println("binary of "+ myNum + " is " + binNum);
+    }
+
+    public static void main(String args[]) {
+        decToBin(11);
+    }
+}
