4452c7dea5b77579ac5a2c0a3b9912e845c6a543
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-07 13:55:29 +0530

    Update 6. arrays.java

--- 6. arrays.java
+++ 6. arrays.java
@@ -89,5 +89,32 @@ public class Arrays {
     }
 }
 ======================================================================================
-  
+  //LINEAR SERACH
+    ex 1
 
+public class JavaMastery {
+
+    public static int linearSearch(int numbers[], int key) {
+
+        for(int i=0; i<numbers.length; i++) {
+            if(numbers[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+       int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+       int key = 190;
+
+       int index = linearSearch(numbers, key);
+       if(index == -1) {
+        System.out.println("Not found");
+       } else {
+        System.out.println("Key is at the index: " + index);
+       }
+    }
+}
+=====================================================================
+ex 2
