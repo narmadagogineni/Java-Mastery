1d9ea30a0743dfa8dde319fdbb5d875f965263a6
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-08 13:24:09 +0530

    Update 7. strings.java

--- 7. strings.java
+++ 7. strings.java
@@ -1,1 +1,43 @@
+//STRINGS
+STRING WITH next();
+
+import java.util.*;
+
+public class stringstut {
+    
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        //strings are IMMUTABLE
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.next();
+        System.out.println(name);
+
+    }
+}
+=================================================================================================
+//String with nextLine()
+  
+import java.util.*;
+
+public class stringstut {
+    
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        //strings are IMMUTABLE
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+
+    }
+}
 
