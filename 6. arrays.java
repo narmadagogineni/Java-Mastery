6a35ac7e242773e14e66f50565acd0da7a937f38
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-08 12:54:00 +0530

    Update 6. arrays.java

--- 6. arrays.java
+++ 6. arrays.java
@@ -279,7 +279,34 @@ public class JavaMastery {
     }
 }
 
+=================================================================================================================
+//Number Pairs
 
+import java.util.*;
+
+public class Arrays {
+    public static void numPairs(int numbers[]) {
+        int totalPairs = 0;
+        for(int i=0; i<numbers.length; i++) {
+            int curr = numbers[i];  //2, 4, 6, 8,10, 12
+            for(int j=i+1; j<numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ") ");
+                totalPairs++;
+            }
+
+            System.out.println();
+        }
+        System.out.println("Total pairs: "+ totalPairs);
+
+    }
+
+
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10, 12};
+        numPairs(numbers);
+
+    }
+}
 
 
     
