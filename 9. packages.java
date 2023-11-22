6f6c85cfd751ca11cc28429a888b160b27afebab
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-11-22 23:35:12 +0530

    Update 9. packages.java

--- 9. packages.java
+++ 9. packages.java
@@ -1,1 +1,21 @@
+// there are 2 types of packages
+//   1 inbuilt packages
+//   2 userdefined packages
 
+//to create user defined package
+
+package myPackage;
+
+import java.util.Scanner;
+
+import java.util.*;
+
+public class Oops {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
+================================================================================
